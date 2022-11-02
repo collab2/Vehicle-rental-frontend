39b6d38f78fc368398a83a86182c4e1b0f702867
mabdullah12101 <muhabdullah12101@gmail.com>
2022-11-03 03:00:25 +0700

    feat: add structure folder, add cdn boostrap

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import PublicRoute from "./utils/routes/publicRoute";
+import PrivateRoute from "./utils/routes/privateRoute";
+import LandingPage from "./pages/LandingPage/index.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* PUBLIC AUTH ROUTE */}
+        <Route element={<PublicRoute />}></Route>
+
+        {/* MAIN */}
+        {/* PRIVATE ROUTE */}
+        <Route element={<PrivateRoute />}></Route>
+
+        {/* PRIVATE ADMIN ROUTE */}
+        <Route element={<PrivateRoute isAdmin={true} />}></Route>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
