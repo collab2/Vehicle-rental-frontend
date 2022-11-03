11af2909c9d1cf184f53d85fd6cebe081c3621f5
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2022-11-03 16:18:48 +0700

    add vehicle type

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PublicRoute from "./utils/routes/publicRoute";
 import PrivateRoute from "./utils/routes/privateRoute";
 import LandingPage from "./pages/LandingPage/index.jsx";
 import History from "./pages/History";
+import VehicleType from "./pages/VehicleType";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route element={<PrivateRoute isAdmin={true} />}></Route>
         <Route path="/" element={<LandingPage />} />
         <Route path="/history" element={<History />} />
+        <Route path="/vehicle-type" element={<VehicleType />} />
       </Routes>
     </BrowserRouter>
   );
