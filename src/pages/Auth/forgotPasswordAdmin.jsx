a50c1e1bb828a659bc959278591adece3215ddcb
mabdullah12101 <muhabdullah12101@gmail.com>
2022-11-06 16:40:52 +0700

    fix: auth page

--- src/pages/Auth/forgotPasswordAdmin.jsx
+++ src/pages/Auth/forgotPasswordAdmin.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import authimage from "../../assets/img/authimage.png";
+// import authimage from "../../assets/img/authimage.png";
 import "./index.css";
-import { Icon } from "@iconify/react";
+// import { Icon } from "@iconify/react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import Logo from "../../component/Logo";
+// import Logo from "../../component/Logo";
 import { forgotPassword } from "../../stores/actions/forgotPassword";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
@@ -42,26 +42,24 @@ export default function ForgotPasswordAdmin() {
   };
 
   return (
-    <div className="container-fluid px-0">
-      <div className="row mx-0">
-        <div className="col-md-6 col-lg-6 col-xl-6 col-6  px-0 mx-0 container-image">
-          <div className="left-side">
-            <img src={authimage} alt="van" className="authimage" />
-          </div>
+    <div className="container-fluid px-0 vh-100">
+      <div className="row mx-0 h-100">
+        <div className="col-md-6 col-lg-6 col-xl-6 col-12 d-none d-md-block px-0 mx-0 container-image h-100">
+          <div className="left-side h-100"></div>
         </div>
-        <div className="col-md-6 col-lg-6 col-xl-6 col-12 right-side d-flex flex-column justify-content-start align-items-start px-0 py-0">
+        <div className="col-md-6 col-lg-6 col-xl-6 col-12 right-side d-flex flex-column justify-content-center px-0 py-0 h-100">
           {/* <div className="mobile-display">
             <img src={authimage} alt="van" className="" />
           </div> */}
-          <div className="desktop-display">
-            <div className="m-5 p-5">
+          <div className="desktop-display h-100 overflow-hidden">
+            <div className="mx-5 px-5 mb-5 mt-5">
               <h1>Forgot Password</h1>
               <h3 className="mb-5">
                 Enter your verified account email and we <br /> will send you a
                 link to reset your password
               </h3>
             </div>
-            <form className="form-login ms-5 ps-5">
+            <form className="form-login mx-5 px-5">
               <input
                 type="email"
                 name="email"
@@ -79,17 +77,11 @@ export default function ForgotPasswordAdmin() {
               </div>
               <h1 className="separator pt-4 mb-5 mt-5">or try another way</h1>
             </form>
-            <div className="m-5 ps-5 form-login">
+            <div className="m-5 px-5 form-login">
               <div className="">
-                <div className="d-grid">
-                  <button className="google-btn-forgot mt-4 mb-3">
-                    <Icon icon="flat-color-icons:google" width="40px" /> Sign up
-                    with Google
-                  </button>
-                </div>
                 <div className="d-grid mt-5">
                   <button
-                    className="sign-btn-forgot mt-4"
+                    className="sign-btn-forgot "
                     onClick={() => handleNavigate("signup")}
                   >
                     Sign Up
@@ -98,47 +90,6 @@ export default function ForgotPasswordAdmin() {
               </div>
             </div>
           </div>
-          <footer className="w-100">
-            <div>
-              <Logo />
-              <p className="font-mulish mt-4">
-                Plan and book your perfect trip with <br /> expert advice,
-                travel tips for vehicle <br /> information from us
-              </p>
-              <p className="mt-5">
-                ©2020 Vehicle Rental Center. All rights reserved
-              </p>
-            </div>
-            <div
-              className="d-flex justify-content-center align-items-center"
-              id="footer-socmed"
-              style={{ columnGap: "1rem", paddingTop: "30px" }}
-            >
-              <div>
-                <Icon icon={"akar-icons:twitter-fill"} width={25} height={25} />
-              </div>
-              <div>
-                <Icon icon={"brandico:facebook"} width={25} height={25} />
-              </div>
-              <div>
-                <Icon
-                  icon={"akar-icons:instagram-fill"}
-                  width={25}
-                  height={25}
-                />
-              </div>
-              <div>
-                <Icon
-                  icon={"akar-icons:linkedin-fill"}
-                  width={25}
-                  height={25}
-                />
-              </div>
-              <div>
-                <Icon icon={"akar-icons:youtube-fill"} width={25} height={25} />
-              </div>
-            </div>
-          </footer>
           <ToastContainer />
         </div>
       </div>
