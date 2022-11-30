e9e93e3557f96d60693ce047ab2240c7f92b0bbd
mabdullah12101 <muhabdullah12101@gmail.com>
2022-11-30 17:00:42 +0700

    feat: update reservation payment and update profile

--- src/pages/Payment/payment.jsx
+++ src/pages/Payment/payment.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../../component/Header";
-// import Bike from "../../assets/img/bike.png";
-import Dropdown from "react-bootstrap/Dropdown";
 import { reserve } from "../../stores/actions/reservation";
-// import Button from "react-bootstrap/Button";
 import { Icon } from "@iconify/react";
 import { useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -12,16 +9,19 @@ import { getUserById } from "../../stores/actions/user";
 import { ToastContainer, toast } from "react-toastify";
 import { getProductById } from "../../stores/actions/product";
 import "react-toastify/dist/ReactToastify.css";
+import moment from "moment";
 
 export default function Payment() {
   const navigate = useNavigate();
   const { state } = useLocation();
+  console.log(useLocation());
+  console.log("state");
   console.log(state);
   const [user, setUser] = useState({});
   const [product, setProduct] = useState({});
   const dispatch = useDispatch();
   const userId = localStorage.getItem("userId");
-  const productId = state.productId;
+  const productId = state?.productId;
   const imageProduct = `https://res.cloudinary.com/dtjeegwiz/image/upload/v1667656027/${product[0]?.image1}`;
   const locationProduct = product[0]?.location;
   console.log(locationProduct);
@@ -59,19 +59,26 @@ export default function Payment() {
   const handleReservation = () => {
     const setData = {
       location: locationProduct,
-      startDate: state.startDate,
-      returnDate: state.returnDate,
-      quantity: state.quantity,
+      startDate: moment(state?.startDate, "YYYY-MM-DD"),
+      returnDate: moment(state?.returnDate, "YYYY-MM-DD"),
+      quantity: state?.quantity,
       userId: userId,
       productId: productId,
-      amount: state.amount,
+      amount: state?.amount,
     };
     dispatch(reserve(setData))
       .then((response) => {
-        console.log(response);
+        console.log(response.action.payload.data.data.redirectUrl.redirect_url);
         toast.success(response.value.data.msg, {
           position: toast.POSITION.TOP_CENTER,
         });
+        setTimeout(() => {
+          navigate("/");
+          window.open(
+            response.action.payload.data.data.redirectUrl.redirect_url,
+            "_blank"
+          );
+        }, [1000]);
       })
       .catch((error) =>
         toast.error(error.response.data.msg, {
@@ -95,7 +102,16 @@ export default function Payment() {
             <span className="ms-5">Payment</span>
           </div>
           <div className="col-md-5 me-5">
-            <img src={imageProduct} className="img-fluid" alt="bike" />
+            <img
+              src={imageProduct}
+              className=""
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                borderRadius: "15px",
+              }}
+              alt="bike"
+            />
           </div>
           <div className="col-md-6 d-flex flex-column justify-content-center gap-4 mb-5 mt-3">
             <span className="payment-item-name mb-2">
@@ -105,61 +121,42 @@ export default function Payment() {
               {product[0]?.location}
             </span>
             <span className="payment-prepayment mb-3">No Prepayment</span>
-            <span className="payment-code-top mb-2">#FG1209878YZS</span>
-            <div className="col-md-6">
-              <button className="payment-copy-button-top">
-                Copy booking code
-              </button>
-            </div>
           </div>
-          <div className="col-md-5 border rounded py-3 px-5 mb-4 me-5">
-            <span className="payment-quantity">
-              Quantity : {state.quantity}
-            </span>
-          </div>
-          <div className="col-md-6 border rounded py-3 px-5 mb-4 d-flex justify-content-between">
-            <span className="payment-quantity">
-              Reservation Date : {state.startDate} - {state.returnDate}
-            </span>
-            <span className="payment-date">{state.firstDate}</span>
-          </div>
-          <div className="col-md-5 border rounded py-3 gap-2 px-5 me-5 d-flex flex-column">
-            <span className="payment-order-identity">Order Details :</span>
-            <span className="payment-order">
-              {state.quantity} {product[0]?.nameproduct} : {product[0]?.price}
-            </span>
-            <span className="mt-3 payment-total">Total : {state.amount}</span>
-          </div>
-          <div className="col-md-6 border rounded py-3 gap-2 px-5 d-flex flex-column justify-content-center">
-            <span className="payment-order-identity">Identity :</span>
-            <span className="payment-identity">
-              User & Phone Number : {user[0]?.name} / {user[0]?.phone}
-            </span>
-            <span className="payment-identity">Email : {user[0]?.email}</span>
-          </div>
-          <div className="col-md-12 d-flex justify-content-between align-items-center pe-5 my-5">
-            <span className="payment-payment-code">Payment code :</span>
-            <div className="col-md-5 border rounded py-3 d-flex flex-row justify-content-between px-3">
-              <span className="payment-code-bottom">#FG1209878YZS</span>
-              <button className="px-4 payment-copy-bottom">Copy</button>
-            </div>
-            <div className="border col-md-4 rounded me-3 py-3 px-5">
-              <Dropdown>
-                <Dropdown.Toggle
-                  className="payment-dropdown"
-                  id="dropdown-basic"
-                >
-                  Select payment methods
-                </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Cash</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Transfer</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+          <div className="row mt-5 px-4">
+            <div className="col-md-5 border rounded px-5 py-4 mb-4 me-5">
+              <span className="payment-quantity">
+                Quantity : {state?.quantity}
+              </span>
+            </div>
+            <div className="col-md-6 border rounded py-4 px-5 mb-4 d-flex align-items-center">
+              <span className="payment-quantity">
+                Reservation Date : {state?.startDate} - {state?.returnDate}
+              </span>
+              <span className="payment-date">{state?.firstDate}</span>
             </div>
           </div>
-          <div className="d-grid gap-2 pe-5 mb-5">
+
+          <div className="row mb-5 px-4">
+            <div className="col-md-5 border rounded gap-2 p-5 me-5 d-flex flex-column">
+              <span className="payment-order-identity">Order Details :</span>
+              <span className="payment-order">
+                {state?.quantity} {product[0]?.nameproduct} : Rp.{" "}
+                {Number(product[0]?.price).toLocaleString()}
+              </span>
+              <span className="mt-3 payment-total">
+                Total : Rp. {state?.amount.toLocaleString()}
+              </span>
+            </div>
+            <div className="col-md-6 border rounded py-3 gap-2 px-5 d-flex flex-column justify-content-center">
+              <span className="payment-order-identity">Identity :</span>
+              <span className="payment-identity">
+                User & Phone Number : {user[0]?.name} / {user[0]?.phone}
+              </span>
+              <span className="payment-identity">Email : {user[0]?.email}</span>
+            </div>
+          </div>
+          <div className="d-grid gap-2 mb-5" style={{ paddingRight: "90px" }}>
             <button
               className="py-3 payment-finish rounded"
               size="lg"
